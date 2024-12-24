591daebb06243b5f078033fa8d1680f55162315f
leemheeje <hjleem@kcp.co.kr>
2024-12-24 15:53:01 +0900

    1.3.0

--- lib/main.ts
+++ lib/main.ts
@@ -2,11 +2,11 @@ import TestLeem from '../src/components/TestLeem.vue'
 
 export default{
     install: (app:any)=>{
-        app.config.globalProperties.$testleem = (s:any)=>{
-            console.log(s);
-            console.log(123);
-            return `hello plugins params : ${s}`
-        }
+        // app.config.globalProperties.$testleem = (s:any)=>{
+        //     console.log(s);
+        //     console.log(123);
+        //     return `hello plugins params : ${s}`
+        // }
         app.component('TestLeem', TestLeem)
     }
 }

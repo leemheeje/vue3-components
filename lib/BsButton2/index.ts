5956c8b8093aa9cb6e89b5591a9e69f3b201c166
NHNKCP-hjleem <hjleem@kcp.co.kr>
2025-02-13 10:43:05 +0900

    test123456

---
+++ lib/BsButton2/index.ts
@@ -0,0 +1,4 @@
+import BsButton2 from '../../src/components/BsButton2/index.vue'
+import type {Props as BsButton2Props} from '../../src/components/BsButton2/index.type'
+
+export {BsButton2, type BsButton2Props}
